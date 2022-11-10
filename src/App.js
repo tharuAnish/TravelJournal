4d9c2f683430cc37a3468c57bc13eacbfb74ca85
tharuAnish <namoanishtharu@gmail.com>
2022-11-10 16:05:12 +0530

    data array created and card initials added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Card from './components/Card';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <h1>This is app</h1>
+      <Card/>
     </div>
   );
 }
